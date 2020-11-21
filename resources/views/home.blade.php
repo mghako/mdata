9802dc79b79a1b1bbccef5d3393f94a7e0939785
mghako <htetaungko@yahoo.com>
2020-11-21 21:28:26 +0630

    change home text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Welcome from MData!
                 </div>
             </div>
         </div>
